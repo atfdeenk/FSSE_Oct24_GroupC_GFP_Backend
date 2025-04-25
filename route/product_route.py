da32d5ead5352fb4d06792ee02218374fec6570d
ferianai <ferianaisti@gmail.com>
2025-04-25 15:29:37 +0700

    no update

--- route/product_route.py
+++ route/product_route.py
@@ -9,7 +9,8 @@ from services.product_services import (
     delete_product_and_return_message,
 )
 
-product_bp = Blueprint('product_bp', __name__)
+product_bp = Blueprint("product_bp", __name__)
+
 
 @product_bp.route("/products", methods=["GET"])
 def get_all_products():
@@ -20,14 +21,13 @@ def get_all_products():
     sort_by = request.args.get("sort_by", default="created_at", type=str)
     sort_order = request.args.get("sort_order", default="desc", type=str)
 
-
     products = get_all_serialized_products(
         search=search,
         category_id=category_id,
         page=page,
         limit=limit,
         sort_by=sort_by,
-        sort_order=sort_order
+        sort_order=sort_order,
     )
 
     return jsonify(products), 200
@@ -40,19 +40,23 @@ def get_product(product_id):
         return jsonify({"message": "Product not found"}), 404
     return jsonify(product), 200
 
+
 @product_bp.route("/products", methods=["POST"])
 @jwt_required()
 @role_required("vendor")
 def create_product():
-    
+
     if not request.is_json:
         print("❌ Not JSON")
         return jsonify({"msg": "Invalid content type"}), 400
     data = request.get_json()
-    
+
     product = create_product_with_serialization(data)
     if not product:
-        return jsonify({"message": "Error creating product (possibly slug conflict)"}), 400
+        return (
+            jsonify({"message": "Error creating product (possibly slug conflict)"}),
+            400,
+        )
     return jsonify(product), 201
 
 
@@ -65,6 +69,7 @@ def update_product(product_id):
     if not product:
         return jsonify({"message": "Product not found"}), 404
     return jsonify(product), 200
+
 
 @product_bp.route("/products/<int:product_id>", methods=["DELETE"])
 @jwt_required()
