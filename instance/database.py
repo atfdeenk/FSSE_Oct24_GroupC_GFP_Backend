bcba9ef5bce9dc53ac5ff15b456a2d942bb748d2
ferianai <ferianaisti@gmail.com>
2025-04-27 08:18:56 +0700

    chore: create a custom subclass of SQLAlchemy in instance/database.py

--- instance/database.py
+++ instance/database.py
@@ -1,18 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-db = SQLAlchemy()
+
+class CustomSQLAlchemy(SQLAlchemy):
+    def create_engine(self, *args, **kwargs):
+        app = self.get_app()
+        engine_options = app.config.get("SQLALCHEMY_ENGINE_OPTIONS", {})
+        kwargs.update(engine_options)
+        return create_engine(*args, **kwargs)
+
+
+db = CustomSQLAlchemy()
 migrate = Migrate()
 
 
 def init_db(app):
-    engine_options = app.config.get("SQLALCHEMY_ENGINE_OPTIONS", {})
-    engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], **engine_options)
-    # Bind the engine to the SQLAlchemy db object
-    db.engine = engine
-
     db.init_app(app)
     migrate.init_app(app, db)
     # Initialize the database and migration engine
