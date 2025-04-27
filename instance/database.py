38bde91a65a9fbe0ec15fcaf1f8df1ab17048135
ferianai <ferianaisti@gmail.com>
2025-04-27 08:16:01 +0700

    chore: Modify instance/database.py to create the engine explicitly using create_engine

--- instance/database.py
+++ instance/database.py
@@ -1,11 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 
 def init_db(app):
+    engine_options = app.config.get("SQLALCHEMY_ENGINE_OPTIONS", {})
+    engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], **engine_options)
+    # Bind the engine to the SQLAlchemy db object
+    db.engine = engine
+
     db.init_app(app)
     migrate.init_app(app, db)
     # Initialize the database and migration engine
