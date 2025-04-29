f8bd13b08a0d28e2fb20cf9312d94cab6e9fe03c
ferianai <ferianaisti@gmail.com>
2025-04-29 23:50:13 +0700

    fix: update add rollback in product_category and cart in repo,services and test to avoid OpeationalError

--- repo/cart_repo.py
+++ repo/cart_repo.py
@@ -1,11 +1,17 @@
 from models.cart import Cart
 from instance.database import db
 
+
 def get_cart_by_user(user_id):
     return Cart.query.filter_by(user_id=user_id).first()
+
 
 def create_cart_for_user(user_id):
     cart = Cart(user_id=user_id)
     db.session.add(cart)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
     return cart
--- repo/product_category_repo.py
+++ repo/product_category_repo.py
@@ -1,6 +1,7 @@
 from instance.database import db
 from models.product_category import ProductCategories
 
+
 def assign_category_to_product(product_id, category_id):
     relation = ProductCategories(product_id=product_id, category_id=category_id)
     db.session.add(relation)
@@ -11,16 +12,21 @@ def assign_category_to_product(product_id, category_id):
         db.session.rollback()
         return None
 
+
 def get_categories_by_product(product_id):
     return ProductCategories.query.filter_by(product_id=product_id).all()
 
+
 def remove_category_from_product(product_id, category_id):
     relation = ProductCategories.query.filter_by(
-        product_id=product_id,
-        category_id=category_id
+        product_id=product_id, category_id=category_id
     ).first()
     if relation:
         db.session.delete(relation)
-        db.session.commit()
-        return True
+        try:
+            db.session.commit()
+            return True
+        except:
+            db.session.rollback()
+            return False
     return False
