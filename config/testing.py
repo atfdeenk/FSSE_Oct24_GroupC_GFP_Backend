72edea0d8fced7bd6044d5a2315dbc3c8dd891da
ferianai <ferianaisti@gmail.com>
2025-04-30 22:05:24 +0700

    fix: config setting for local, production and testing

--- config/testing.py
+++ config/testing.py
@@ -1,18 +1,10 @@
 # config/testing.py
 import os
 
-# TESTING = True
-# DB_HOST = "localhost"
-# DB_NAME = "test_db"
-# SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+TESTING = True
+DB_HOST = "localhost"
+DB_NAME = "test_db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
 
-# # Add the JWT secret key (symmetric key for HS256)
-# JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "testing-secret-key")
-
-# import os
-
-
-class TestingConfig:
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "testing-secret-key")
+# Add the JWT secret key (symmetric key for HS256)
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "testing-secret-key")
